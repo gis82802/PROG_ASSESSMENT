1012e79d5a798733766006560e0fc597f5960869
justin8120 <justin940218@gmail.com>
2025-07-23 16:29:21 +0800

    Add files via upload

---
+++ 114C暑修資料/7_22/00000007/00000013/homework/00000186/Q1/Chenn.java
@@ -0,0 +1,8 @@
+public class Chenn
+{
+   public static void main(String args[])
+   {
+      System.out.println("NFU is good");
+      System.out.println("資管系 陳邦譽");
+   }
+}
---
+++ 114C暑修資料/7_22/00000007/00000013/homework/00000187/Q1/Ch1_2.java
@@ -0,0 +1,7 @@
+public class Ch1_2
+{
+  public static void main (String[] args){
+     System.out.println("NFU is good");
+     System.out.println("我是孫益康");
+  }
+}
