2e1fc6715792388109fd7e587e2d4a40c39a9266
Shinatsu <144865080+Shinatsu94@users.noreply.github.com>
2025-08-20 18:14:20 +0800

    Add files via upload

---
+++ test.java
@@ -0,0 +1,11 @@
+/*
+    function: Java first program
+    author"   WJ Lin
+    date:     July 22, 2025
+    version:  0.1
+*/
+public class Hello{
+    public static void  main(String[] args){
+        System.out.println("NFU is good");
+    }
+}
