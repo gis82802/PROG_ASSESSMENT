2f3b521e32db2994819ac5b2342ed8565349bb83
Shinatsu <144865080+Shinatsu94@users.noreply.github.com>
2025-08-29 17:52:05 +0800

    Add files via upload

---
+++ LingoBridge/data/sample/test.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class hw4_2
+{
+   public static void main(String args[])
+   {
+      for(int i=1;i<=9;i+=2)
+      {
+         for(int j=2;j<=8;j+=2)
+         {
+            System.out.print(i + "*" + j + "=" + i*j);
+            if(j != 8) System.out.print("\t");
+         }
+         System.out.println();
+      }
+   }
+}
